4ae5fb99b540ad7635dd094c0e60091ea4a62792
Amal <amalshaiju16@gmail.com>
2021-01-21 05:14:27 -0500

    updatedjavascript to change backrgound color of nav-item-box-two

--- wwwroot/js/nav.js
+++
@@ -1,9 +0,0 @@
-﻿// Amal shaiju 20-01-20/
-
-
-var home = document.getElementById("home");
-
-links.forEach(e => e.addEventListener("click", function () {
-    document.getElementById("home").innerHTML = "Hello World";))
-
-
