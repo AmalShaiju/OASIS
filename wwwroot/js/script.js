b0cda06fcf2b3e8cd9e1444481f42a4949872079
Amal <amalshaiju16@gmail.com>
2021-01-23 07:35:22 -0500

    Added More options Functionality

---
+++ wwwroot/js/script.js
@@ -0,0 +1,11 @@
+﻿// Amal Shaiju 2021-01-23
+
+function divVisibility(i) {
+    console.log(document.getElementById(i+"-ddl"));
+
+    if (document.getElementById(i+"-ddl").style.display != "block")
+           document.getElementById(i+"-ddl").style.display = "block";
+        else
+            document.getElementById(i+"-ddl").style.display = "None";
+
+}
