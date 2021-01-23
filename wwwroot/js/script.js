dca321e1b60413a48013fc92971dcf9df59cef3a
Amal <amalshaiju16@gmail.com>
2021-01-23 09:00:02 -0500

    Cleaned up js

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,24 +1,20 @@
 ﻿// Amal Shaiju 2021-01-23
-
-var options = document.getElementsByClassName(".index-drop-down-menu");
-
-console.log(options.length)
-var visibleDivId;
-
 function divVisibility(i) {
 
     var menu = document.getElementsByClassName("index-drop-down-menu");
 
+    // Closes the div that are shown except the ones that's clicked
     for (var j = 0; j < menu.length; j++) {
 
         console.log(menu[j].id, i)
         if (menu[j].id != i + "-ddl") {
-
+           
             menu[j].style.display = "None";
 
         }
     }
 
+   // If the clicked div is hidden show it
     if (document.getElementById(i + "-ddl").style.display != "block") {
         document.getElementById(i + "-ddl").style.display = "block";
     }
