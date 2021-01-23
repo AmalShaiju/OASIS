1873181f3faba1db24e67c7e386de8b087fc9156
Amal <amalshaiju16@gmail.com>
2021-01-23 08:49:38 -0500

    Fixed opening and closing bug of more option functionality

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,11 +1,30 @@
 ﻿// Amal Shaiju 2021-01-23
 
-function divVisibility(i) {
-    console.log(document.getElementById(i+"-ddl"));
+var options = document.getElementsByClassName(".index-drop-down-menu");
 
-    if (document.getElementById(i+"-ddl").style.display != "block")
-           document.getElementById(i+"-ddl").style.display = "block";
-        else
-            document.getElementById(i+"-ddl").style.display = "None";
+console.log(options.length)
+var visibleDivId;
+
+function divVisibility(i) {
+
+    var menu = document.getElementsByClassName("index-drop-down-menu");
+
+    for (var j = 0; j < menu.length; j++) {
+
+        console.log(menu[j].id, i)
+        if (menu[j].id != i + "-ddl") {
+
+            menu[j].style.display = "None";
+
+        }
+    }
+
+    if (document.getElementById(i + "-ddl").style.display != "block") {
+        document.getElementById(i + "-ddl").style.display = "block";
+    }
+
+    else {
+        document.getElementById(i + "-ddl").style.display = "None";
+    }
 
 }
