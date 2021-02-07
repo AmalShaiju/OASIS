af83266bdc941886c46c0893de85decd0a3fc66b
Amal <amalshaiju16@gmail.com>
2021-02-07 10:39:12 -0500

    Completed changes for publish

--- wwwroot/js/headerMenu.js
+++ wwwroot/js/headerMenu.js
@@ -9,14 +9,14 @@ console.log(url)
 if (url.includes("employees")) {
     var homeSideBar = document.getElementById("employee-side-bar").style.backgroundColor = "#7460ee";;
     var home = document.getElementById("employee").style.backgroundColor = "rgb(116, 96, 238, 0.2)";
-    var employeeLogo = document.getElementById("employeeLogo").src = "/resources/employee-alt.png";
+    $("#employeeLogo path").css({ "fill": "#7460ee" });
     var text = document.getElementById("v-pills-employee-tab").style.color = "#7460ee";
     var pagemsg = document.getElementById("page-msg").innerHTML = "Employees";
 }
 else if (url.includes("customers")) {
     var homeSideBar = document.getElementById("customer-side-bar").style.backgroundColor = "#7460ee";;
     var home = document.getElementById("customer").style.backgroundColor = "rgb(116, 96, 238, 0.2)";
-    var CustomerLogo = document.getElementById("customerLogo").src = "/resources/user-alt.png";
+    $("#customerLogo path").css({ "fill": "#7460ee" });
     var text = document.getElementById("v-pills-customer-tab").style.color = "#7460ee";
     var pagemsg = document.getElementById("page-msg").innerHTML = "Customers";
 
@@ -24,7 +24,7 @@ else if (url.includes("customers")) {
 else if (url.includes("projects")) {
     var homeSideBar = document.getElementById("project-side-bar").style.backgroundColor = "#7460ee";;
     var home = document.getElementById("project").style.backgroundColor = "rgb(116, 96, 238, 0.2)";
-    var projectLogo = document.getElementById("projectLogo").src = "/resources/project-alt.png"
+    $("#projectLogo path").css({ "fill": "#7460ee" });
     var text = document.getElementById("v-pills-projct-tab").style.color = "#7460ee";
     var pagemsg = document.getElementById("page-msg").innerHTML = "Projects";
 
@@ -32,14 +32,14 @@ else if (url.includes("projects")) {
 else if (url.includes("roles")) {
     var homeSideBar = document.getElementById("role-side-bar").style.backgroundColor = "#7460ee";
     var home = document.getElementById("role").style.backgroundColor = "rgb(116, 96, 238, 0.2)";
-    var roleLogo = document.getElementById("roleLogo").src = "/resources/roles-alt.png"
     var text = document.getElementById("v-pills-role-tab").style.color = "#7460ee";
+    $("#roleLogo path").css({ "fill": "#7460ee" });
     var pagemsg = document.getElementById("page-msg").innerHTML = "Roles";
 
 }
 else {
     var homeSideBar = document.getElementById("home-side-bar").style.backgroundColor = "#7460ee";;
-    var homeLogo = document.getElementById("homeLogo").src = "/resources/dash-alt1.png";
+    $("#homeLogo path").css({ "fill": "#7460ee" });
     var home = document.getElementById("home").style.backgroundColor = "rgb(116, 96, 238, 0.2)";
     var text = document.getElementById("v-pills-home-tab").style.color = "#7460ee";
     var pagemsg = document.getElementById("page-msg").innerHTML = "Dashboard";
