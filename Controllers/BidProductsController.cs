5290f4486e3f32fd632dfee2fd81c479170faf08
Amal <amalshaiju16@gmail.com>
2021-02-20 17:57:00 -0500

    Added initial Seed Data for all new tables

---
+++ Controllers/BidProductsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OASIS.Data;
+using OASIS.Models;
+
+namespace OASIS.Controllers
+{
+    public class BidProductsController : Controller
+    {
+        private readonly OasisContext _context;
+
+        public BidProductsController(OasisContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BidProducts
+        public async Task<IActionResult> Index()
+        {
+            var oasisContext = _context.BidProducts.Include(b => b.Bid).Include(b => b.Product);
+            return View(await oasisContext.ToListAsync());
+        }
+
+        // GET: BidProducts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bidProduct = await _context.BidProducts
+                .Include(b => b.Bid)
+                .Include(b => b.Product)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (bidProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(bidProduct);
+        }
+
+        // GET: BidProducts/Create
+        public IActionResult Create()
+        {
+            ViewData["BidID"] = new SelectList(_context.Bids, "ID", "ID");
+            ViewData["ProductID"] = new SelectList(_context.Products, "ID", "Code");
+            return View();
+        }
+
+        // POST: BidProducts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Quantity,BidID,ProductID")] BidProduct bidProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bidProduct);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BidID"] = new SelectList(_context.Bids, "ID", "ID", bidProduct.BidID);
+            ViewData["ProductID"] = new SelectList(_context.Products, "ID", "Code", bidProduct.ProductID);
+            return View(bidProduct);
+        }
+
+        // GET: BidProducts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bidProduct = await _context.BidProducts.FindAsync(id);
+            if (bidProduct == null)
+            {
+                return NotFound();
+            }
+            ViewData["BidID"] = new SelectList(_context.Bids, "ID", "ID", bidProduct.BidID);
+            ViewData["ProductID"] = new SelectList(_context.Products, "ID", "Code", bidProduct.ProductID);
+            return View(bidProduct);
+        }
+
+        // POST: BidProducts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Quantity,BidID,ProductID")] BidProduct bidProduct)
+        {
+            if (id != bidProduct.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bidProduct);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BidProductExists(bidProduct.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BidID"] = new SelectList(_context.Bids, "ID", "ID", bidProduct.BidID);
+            ViewData["ProductID"] = new SelectList(_context.Products, "ID", "Code", bidProduct.ProductID);
+            return View(bidProduct);
+        }
+
+        // GET: BidProducts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bidProduct = await _context.BidProducts
+                .Include(b => b.Bid)
+                .Include(b => b.Product)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (bidProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(bidProduct);
+        }
+
+        // POST: BidProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bidProduct = await _context.BidProducts.FindAsync(id);
+            _context.BidProducts.Remove(bidProduct);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BidProductExists(int id)
+        {
+            return _context.BidProducts.Any(e => e.ID == id);
+        }
+    }
+}
--- Data/OASeedData.cs
+++ Data/OASeedData.cs
@@ -12,6 +12,9 @@ namespace OASIS.Data
     {
         public static void Initialize(IServiceProvider serviceProvider)
         {
+            Random random = new Random();
+
+            string[] baconNotes = new string[] { "Bacon ipsum dolor amet meatball corned beef kevin, alcatra kielbasa biltong drumstick strip steak spare ribs swine. Pastrami shank swine leberkas bresaola, prosciutto frankfurter porchetta ham hock short ribs short loin andouille alcatra. Andouille shank meatball pig venison shankle ground round sausage kielbasa. Chicken pig meatloaf fatback leberkas venison tri-tip burgdoggen tail chuck sausage kevin shank biltong brisket.", "Sirloin shank t-bone capicola strip steak salami, hamburger kielbasa burgdoggen jerky swine andouille rump picanha. Sirloin porchetta ribeye fatback, meatball leberkas swine pancetta beef shoulder pastrami capicola salami chicken. Bacon cow corned beef pastrami venison biltong frankfurter short ribs chicken beef. Burgdoggen shank pig, ground round brisket tail beef ribs turkey spare ribs tenderloin shankle ham rump. Doner alcatra pork chop leberkas spare ribs hamburger t-bone. Boudin filet mignon bacon andouille, shankle pork t-bone landjaeger. Rump pork loin bresaola prosciutto pancetta venison, cow flank sirloin sausage.", "Porchetta pork belly swine filet mignon jowl turducken salami boudin pastrami jerky spare ribs short ribs sausage andouille. Turducken flank ribeye boudin corned beef burgdoggen. Prosciutto pancetta sirloin rump shankle ball tip filet mignon corned beef frankfurter biltong drumstick chicken swine bacon shank. Buffalo kevin andouille porchetta short ribs cow, ham hock pork belly drumstick pastrami capicola picanha venison.", "Picanha andouille salami, porchetta beef ribs t-bone drumstick. Frankfurter tail landjaeger, shank kevin pig drumstick beef bresaola cow. Corned beef pork belly tri-tip, ham drumstick hamburger swine spare ribs short loin cupim flank tongue beef filet mignon cow. Ham hock chicken turducken doner brisket. Strip steak cow beef, kielbasa leberkas swine tongue bacon burgdoggen beef ribs pork chop tenderloin.", "Kielbasa porchetta shoulder boudin, pork strip steak brisket prosciutto t-bone tail. Doner pork loin pork ribeye, drumstick brisket biltong boudin burgdoggen t-bone frankfurter. Flank burgdoggen doner, boudin porchetta andouille landjaeger ham hock capicola pork chop bacon. Landjaeger turducken ribeye leberkas pork loin corned beef. Corned beef turducken landjaeger pig bresaola t-bone bacon andouille meatball beef ribs doner. T-bone fatback cupim chuck beef ribs shank tail strip steak bacon." };
 
             using (var context = new OasisContext(
                 serviceProvider.GetRequiredService<DbContextOptions<OasisContext>>()))
@@ -43,7 +46,13 @@ namespace OASIS.Data
                          Name = "Botanist",
                          LabourCostPerHr = Convert.ToDecimal(50.0),
                          LabourPricePerHr = Convert.ToDecimal(75.0)
-                     }
+                     },
+                      new Role
+                      {
+                          Name = "Sales Associate",
+                          LabourCostPerHr = Convert.ToDecimal(50.0),
+                          LabourPricePerHr = Convert.ToDecimal(75.0)
+                      }
                 );
                     context.SaveChanges();
                 }
@@ -114,7 +123,20 @@ namespace OASIS.Data
                         Phone = 9255554213,
                         Email = "Rgonz1@outlook.com",
                         RoleID = context.Roles.FirstOrDefault(d => d.Name == "Botanist").ID
-                    });
+                    },
+                     new Employee
+                     {
+                         FirstName = "Dave",
+                         LastName = "Kendell",
+                         AddressLineOne = "580 First Ave",
+                         City = "Welland",
+                         Province = "Ontario",
+                         Country = "Canada",
+                         Phone = 9255524213,
+                         Email = "Dkendell1@outlook.com",
+                         RoleID = context.Roles.FirstOrDefault(d => d.Name == "Sales Associate").ID
+                     }
+                    );
                     context.SaveChanges();
                 }
 
@@ -300,7 +322,144 @@ namespace OASIS.Data
                     context.SaveChanges();
                 }
 
+
+                string[] productTypes = new string[] { "Plant Inventory", "Pottery Inventory", "Materials Inventory"};
+                if (!context.ProductTypes.Any())
+                {
+                   foreach( string i in productTypes)
+                    {
+                        ProductType a = new ProductType()
+                        {
+                            Name = i
+                        };
+                        context.Add(a);
+                    }
+                    context.SaveChanges();
+
+                }
+
+                if (!context.Products.Any())
+                {
+                    context.Products.AddRange(
+                    new Product
+                    {
+                        Code = "Lacco",
+                        Description = "Lacco Australasica",
+                        size = "15 Gal",
+                        Price = 450.00,
+                        ProductTypeID = context.ProductTypes.SingleOrDefault(p => p.Name == "Plant Inventory").ID
+
+                    },
+                      new Product
+                      {
+                          Code = "TCP50",
+                          Description = "t/c Pot",
+                          size = "50 Gal",
+                          Price = 110.95,
+                          ProductTypeID = context.ProductTypes.SingleOrDefault(p => p.Name == "Pottery Inventory").ID
+
+                      },
+                        new Product
+                        {
+                            Code = "CBRK5",
+                            Description = "Decorative Cedar Bark",
+                            size = "bag(5 cu Ft)",
+                            Price = 15.95,
+                            ProductTypeID = context.ProductTypes.SingleOrDefault(p => p.Name == "Materials Inventory").ID
+
+                        });
+                    context.SaveChanges();
+                }
+
+                string[] bidStatus = new string[] { "Requires Approval", "Approved", "Disapproved" };
+                if (!context.BidStatuses.Any())
+                {
+                    foreach (string i in bidStatus)
+                    {
+                        BidStatus a = new BidStatus()
+                        {
+                            Name = i
+                        };
+                        context.Add(a);
+                    }
+                    context.SaveChanges();
+                }
+
+                int[] projectID = context.Projects.Select(s => s.ID).ToArray();
+                int[] designerID = context.Employees.Where(p => p.Role.Name == "Designer").Select(p => p.ID).ToArray();
+                int[] salesAssociateID = context.Employees.Where(p => p.Role.Name == "Sales Associate").Select(p => p.ID).ToArray();
+                int[] bidStatusID = context.BidStatuses.Select(p => p.ID).ToArray();
+                int ProjectCount = projectID.Count();
+                int designerCount = designerID.Count();
+                int bidStatusCount = bidStatusID.Count();
+                int salesAssociateCount = salesAssociateID.Count();
+
+                if (!context.Bids.Any())
+                {
+                    foreach (int i in projectID)
+                    {
+                        Bid a = new Bid()
+                        {
+                            DateCreated = DateTime.Now,
+                            EstAmount = random.Next(1000, 100000),
+                            EstBidStartDate = DateTime.Today,
+                            EstBidEndDate = DateTime.Today.AddDays(random.Next(5)),
+                            comments = baconNotes[random.Next(5)],
+                            DesignerID = designerID[random.Next(designerCount)],
+                            SalesAsscociateID = salesAssociateID[random.Next(salesAssociateCount)],
+                            ProjectID = projectID[random.Next(ProjectCount)],
+                            BidStatusID = bidStatusID[random.Next(bidStatusCount)],
+                            
+
+                        };
+                        context.Bids.Add(a);
+
+                    } 
+                    context.SaveChanges();
+                }
+
+                int[] bidID = context.Bids.Select(s => s.ID).ToArray();
+                int[] productID = context.Products.Select(p => p.ID).ToArray();
+                if (!context.BidProducts.Any())
+                {
+                    foreach(int i in bidID)
+                    {
+
+                        BidProduct a = new BidProduct()
+                        {
+                            Quantity = random.Next(5,100),
+                            BidID = bidID[random.Next(bidID.Count())],
+                            ProductID = productID[random.Next(productID.Count())]
+                        };
+                        context.BidProducts.Add(a);
+
+                    }
+                    context.SaveChanges();
+                }
+
+                int[] roleID = context.Roles.Select(p => p.ID).ToArray();
+                if (!context.BidLabours.Any())
+                {
+                    foreach (int i in bidID)
+                    {
+
+                        BidLabour a = new BidLabour()
+                        {
+                            Hours = random.Next(1, 8),
+                            Description = baconNotes[random.Next(5)],
+                            RoleID = roleID[random.Next(roleID.Count())],
+                            BidID = bidID[random.Next(bidID.Count())]
+
+                        };
+                        context.BidLabours.Add(a);
+
+                    }
+                    context.SaveChanges();
+                }
             }
+
+
         }
     }
+
 }
