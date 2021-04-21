45dfd455fba5d021bdc63482e99190c5653e2c66
Amal Shaiju <65841904+AmalShaiju@users.noreply.github.com>
2021-04-21 13:06:55 -0400

    Final Commit for final presenetation

---
+++ obj/Release/netcoreapp3.1/win-x86/PubTmp/Out/wwwroot/js/autofill.js
@@ -0,0 +1,58 @@
+﻿//Amal shaiju 2021-04-20
+
+function EmployeeFill() {
+    $('#FirstName').val("Amal");
+    $('#LastName').val("Shaiju");
+    $('#Phone').val("9037216522");
+    $('#Email').val("ASHAIJU1@ncstudents.niagaracollege.ca");
+    $('#AddressLineOne').val("586 First Avenue");
+    $('#City').val("Welland");
+    $('#Province').val("Ontario");
+    $('#Country').val("Canada");
+}
+
+function CustomerFill() {
+    $('#FirstName').val("Bill");
+    $('#MiddleName').val("Rich");
+    $('#LastName').val("Gates");
+    $('#OrgName').val("Microsoft");
+    $('#Position').val("Founder");
+    $('#Phone').val("2898230814");
+    $('#Email').val("Bgates@rich.com");
+    $('#AddressLineOne').val("1180 Raymond Blvd");
+    $('#AddressLineTwo').val(" NJ 07102");
+    $('#City').val("Newark");
+    $('#Province').val("New York");
+    $('#Country').val("United States");
+}
+
+
+function ProjectFill() {
+    $('#Name').val("Microsoft Pod Design");
+    $('#SiteAddressLineOne').val("Microsoft Hub");
+    $('#SiteAddressLineTwo').val("Pod 2");
+    $('#City').val("California");
+    $('#Province').val("New York");
+    $('#Country').val("USA");
+}
+
+function AdminLogin() {
+    $('#txtUserName').val("admin1@outlook.com");
+    $('#txtPassword').val("password");
+}
+
+
+function SalesLogin() {
+    $('#txtUserName').val("bob@outlook.com");
+    $('#txtPassword').val("password");
+}
+
+function Designer() {
+    $('#txtUserName').val("Tamara@outlook.com");
+    $('#txtPassword').val("password");
+}
+
+function ManagementLogin() {
+    $('#txtUserName').val("Cheryl@outlook.com");
+    $('#txtPassword').val("password");
+}
